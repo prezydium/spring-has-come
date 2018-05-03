095f4eec14072c5b52be4c1d08a890b666594bac
Krzysztof Syska <prezydium834@gmail.com>
2018-05-03 09:57:34 +0200

    Initial Commit

---
+++ src/main/java/org/prezydium/springhascome/SpringHasComeApplication.java
@@ -0,0 +1,12 @@
+package org.prezydium.springhascome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHasComeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHasComeApplication.class, args);
+	}
+}
