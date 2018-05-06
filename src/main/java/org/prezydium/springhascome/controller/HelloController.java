c039bf4cc0f5e3d4fc4788d6472b9126da9ac223
Krzysztof Syska <prezydium834@gmail.com>
2018-05-06 19:50:52 +0200

    Learning about Application Context

--- src/main/java/org/prezydium/springhascome/controller/HelloController.java
+++ src/main/java/org/prezydium/springhascome/controller/HelloController.java
@@ -1,15 +1,36 @@
 package org.prezydium.springhascome.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    ApplicationContext localApplicationContext;
+
+    @Autowired
+    public HelloController(ApplicationContext applicationContext){
+        this.localApplicationContext =applicationContext;
+    }
+
     @RequestMapping("/hello")
     public String helloMessage(){
 
         return "HELLO";
+    }
+
+    @RequestMapping("/context")
+    public String checkApplicationContext(){
+        String[] show =  localApplicationContext.getBeanDefinitionNames();
+        StringBuilder sb = new StringBuilder("");
+        for (String s : show ) {
+            sb.append(s + "\n");
+
+        }
+        return sb.toString();
     }
 
 }
