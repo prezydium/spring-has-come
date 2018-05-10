ed8b77a36b0053336e85eee7807ea3e2b3266a37
Krzysztof Syska <prezydium834@gmail.com>
2018-05-10 19:21:08 +0200

    Learning about logs

---
+++ src/main/java/org/prezydium/springhascome/controller/LogSpamController.java
@@ -0,0 +1,21 @@
+package org.prezydium.springhascome.controller;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LogSpamController {
+
+    private final Logger logger = LoggerFactory.getLogger(LogSpamController.class);
+
+    @RequestMapping("/")
+    public String spamLogs(){
+        for (int i =0; i < 10000; i++){
+            logger.error("MSG : " + i);
+        }
+        return "spammed";
+    }
+}
